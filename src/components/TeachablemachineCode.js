362d63e28b72f02840c249a1ed7a4456db26596a
S-MW <72529306+S-MW@users.noreply.github.com>
2022-02-19 05:14:15 +0300

    Change the DataSet to Fruits & show the prediction data

--- src/components/TeachablemachineCode.js
+++ src/components/TeachablemachineCode.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as tf from '@tensorflow/tfjs';
 import * as tmImage from '@teachablemachine/image';
 
+const listOfFruits = [
+    { "name": "apple", "price": "20SR", "description": "An apple is an edible fruit produced by an apple tree." },
+    { "name": "mango", "price": "20SR", "description": "A mango is a sweet tropical fruit, and it's also the name of the trees on which the fruit grows. Ripe mangoes are juicy." },
+    { "name": "orange", "price": "20SR", "description": "Oranges are round orange-coloured fruit that grow on a tree which can reach 10 metres (33 ft) high." },
+    { "name": "strawberry", "price": "20SR", "description": "A strawberry is both a low-growing, flowering plant and also the name of the fruit that it produces. Strawberries are soft, sweet, bright red berries." },
+    { "name": "grape", "price": "15SR", "description": "Grapes are fleshy, rounded fruits that grow in clusters made up of many fruits of greenish, yellowish or purple skin." }
+]
+
 function TeachablemachineCode() {
+
+    const [result, setResult] = useState({ "name": "Test", "price": "Test", "description": "TestTestTestTestTestTestTestTestTestTestTestTestTestTest" });
+
     // the link to your model provided by Teachable Machine export panel
-    const URL = "https://teachablemachine.withgoogle.com/models/yfSjIRARl/";
+    const URL = "https://teachablemachine.withgoogle.com/models/XB9TPbR3H/";
 
     let model, webcam, labelContainer, maxPredictions;
 
@@ -53,8 +64,15 @@ function TeachablemachineCode() {
         for (let i = 0; i < maxPredictions; i++) {
             const classPrediction =
                 prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+            if (prediction[i].probability.toFixed(2) >= 0.80) {
+                setResult(findTheFruits(prediction[i].className));
+            }
             labelContainer.childNodes[i].innerHTML = classPrediction;
         }
+    }
+
+    function findTheFruits(fruitNmae) {
+        return listOfFruits.find(o => o.name === fruitNmae);
     }
 
     return (
@@ -63,6 +81,12 @@ function TeachablemachineCode() {
             <button type="button" onClick={() => { init() }}>Start</button>
             <div id="webcam-container"></div>
             <div id="label-container"></div>
+            <div>
+                <h1>Result :</h1>
+                <h4>Name:{result.name}</h4>
+                <h4>Price:{result.price}</h4>
+                <h4>Description:{result.description}</h4>
+            </div>
         </>
     );
 };
