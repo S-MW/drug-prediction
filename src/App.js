19e0fb6b908b1eeafd623468ba946d4a3c0739ea
S-MW <72529306+S-MW@users.noreply.github.com>
2022-02-19 23:10:33 +0300

    Add link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import TeachablemachineCode from './components/TeachablemachineCode';
 import CallToActionWithAnnotation from './components/CallToActionWithAnnotation'
+import { Link } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+
 function App() {
 
     return (
         <div>
-            <CallToActionWithAnnotation/>
+            <CallToActionWithAnnotation />
             {/* <TeachablemachineCode /> */}
+            <Link ml="600px" href='https://github.com/S-MW' isExternal>
+            Developed by Saad <ExternalLinkIcon mx='2px' />
+            </Link>
         </div>
     );
 }
